501075e2ee15ea8805bfda6049b82fbc2910c291
muhfatih <muhammadfatih4994@gmail.com>
2022-08-07 10:35:55 +0700

    add create table and ending pool connection

--- queries.js
+++ queries.js
@@ -1,47 +1,93 @@
 const Pool = require('pg').Pool
-const pool = new Pool({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'desagantang',
-	password: 'Fatih1607',
-	port: 5432,
-})
+
+const createTables = (req, res) => {
+	const pool = new Pool({
+		user: 'qduwpdhrtgrbkp',
+		host: 'ec2-44-193-178-122.compute-1.amazonaws.com',
+		database: 'desagantangd1d4uabd9b2oh3',
+		password: '79b65bb95ec9b8e8a8633def37c42f48742fe5323b5b4a39ee7ff92d73167ad6',
+		port: 5432,
+	})
+	pool.query('CREATE TABLE Komoditas(KomoditasID integer, Nama text, 	NamaIlmiah text, Deskripsi text, Foto text', (err, res) => {
+		console.log(err);
+	})
+	pool.query('CREATE TABLE Umkm(UmkmID integer, Nama text, DeskripsiSingkat text, Foto text', (err, res) => {
+		console.log(err);
+	})
+	// pool.query('CREATE TABLE Blog(BlogID integer, Nama text, NamaIlmiah text, Deskripsi text, Foto text', (err, res) => {
+	// 	console.log(err);
+	// })
+
+}
 
 const getKomoditas = (req, res) => {
+	const pool = new Pool({
+		user: 'qduwpdhrtgrbkp',
+		host: 'ec2-44-193-178-122.compute-1.amazonaws.com',
+		database: 'desagantangd1d4uabd9b2oh3',
+		password: '79b65bb95ec9b8e8a8633def37c42f48742fe5323b5b4a39ee7ff92d73167ad6',
+		port: 5432,
+	})
 	pool.query('SELECT * FROM komoditas ORDER BY KomoditasId ASC', (err, results) => {
 		if (err) {
 			throw err;
 		}
+		pool.end();
 		res.status(200).json(results.rows)
 	})
 }
 
 const getKomoditasById = (req, res) => {
 	const id = parseInt(req.params.id)
+	const pool = new Pool({
+		user: 'qduwpdhrtgrbkp',
+		host: 'ec2-44-193-178-122.compute-1.amazonaws.com',
+		database: 'desagantangd1d4uabd9b2oh3',
+		password: '79b65bb95ec9b8e8a8633def37c42f48742fe5323b5b4a39ee7ff92d73167ad6',
+		port: 5432,
+	})
 	pool.query('SELECT * FROM komoditas WHERE KomoditasId = $1', [id], (err, results) => {
 		if (err) {
-			throw err
+			throw err;
 		}
 		res.status(200).json(results.rows)
+		pool.end();
 	})
 }
 
-const getUmkmById = (req, res) => {
-	const id = parseInt(req.params.id)
-	pool.query('SELECT * FROM umkm WHERE UmkmId = $1', [id], (err, results) => {
-		if (err) {
-			throw err
-		}
-		res.status(200).json(results.rows)
-	})
-}
 
 const getUmkm = (req, res) => {
+	const pool = new Pool({
+		user: 'qduwpdhrtgrbkp',
+		host: 'ec2-44-193-178-122.compute-1.amazonaws.com',
+		database: 'desagantangd1d4uabd9b2oh3',
+		password: '79b65bb95ec9b8e8a8633def37c42f48742fe5323b5b4a39ee7ff92d73167ad6',
+		port: 5432,
+	})
 	pool.query('SELECT * FROM umkm', (err, results) => {
 		if (err) {
 			throw err
 		}
 		res.status(200).json(results.rows)
+		pool.end()
+	})
+}
+
+const getUmkmById = (req, res) => {
+	const id = parseInt(req.params.id)
+	const pool = new Pool({
+		user: 'qduwpdhrtgrbkp',
+		host: 'ec2-44-193-178-122.compute-1.amazonaws.com',
+		database: 'desagantangd1d4uabd9b2oh3',
+		password: '79b65bb95ec9b8e8a8633def37c42f48742fe5323b5b4a39ee7ff92d73167ad6',
+		port: 5432,
+	})
+	pool.query('SELECT * FROM umkm WHERE UmkmId = $1', [id], (err, results) => {
+		if (err) {
+			throw err
+		}
+		res.status(200).json(results.rows)
+		pool.end()
 	})
 }
 
