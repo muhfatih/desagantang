5eced1076324394d9152598176c0bdc62e6948e3
muhfatih <muhammadfatih4994@gmail.com>
2022-08-07 10:39:56 +0700

    fix endpoint

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/komoditas', db.getKomoditas)
-app.get('/komoditas:id', db.getKomoditasById)
+app.get('/komoditas/:id', db.getKomoditasById)
 app.get('/umkm', db.getUmkm)
-app.get('/umkm:id', db.getUmkmId)
+app.get('/umkm/:id', db.getUmkmId)
 
 app.listen(8080, () => {
 	console.log("running on port 8080");
